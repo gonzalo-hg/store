069e462aee8adca91bb454c7822115c03513cfde
Gonzalo Hernandez <gonzalo.hg19@gmail.com>
2024-12-24 13:04:32 -0600

    feat: ciclo de vida de los componentes

---
+++ src/app/domains/shared/componentes/counter/counter.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  imports: [],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css'
+})
+export class CounterComponent {
+
+  /**
+   * Ver el ciclo de vida con esots input
+   */
+  @Input({required:true }) duration: number = 0;
+  @Input({required:true }) message = '';//esto representa el motor de inferencia de ts y el tipado de angular
+
+  constructor() {
+    //NO ASYNC
+    //before render
+    console.log('Constructor');
+    console.log('-'.repeat(10));
+  }
+
+  ngOnchanges(changes: SimpleChanges){
+    //before and during render
+    console.log('ngOnchanges');
+    console.log('-'.repeat(10));
+    console.log(changes)
+  }
+
+}
